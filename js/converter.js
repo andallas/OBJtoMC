5246eceff0a340a74098d09b4d3ee4d4591c8cc8
Justin Hammond <justin.35f@gmail.com>
2013-08-29 01:54:07 -0700

    Finished script.

--- js/converter.js
+++ js/converter.js
@@ -16,12 +16,12 @@ var converter = (function(converter)
 		}
 
 		regex = /^vt\s(.+)\s(.+).$/;
-		var UVs = [];
+		var uvs = [];
 		for(var i = 0; i < match.length; i++)
 		{
 			var uv = match[i].match(regex);
 			if(uv !== null)
-				UVs.push(uv);
+				uvs.push(uv);
 		}
 
 		regex = /^f\s(.+)\/(.+)\s(.+)\/(.+)\s(.+)\/(.+)\s(.+)\/(.+)$/;
@@ -33,8 +33,126 @@ var converter = (function(converter)
 				faces.push(face);
 		}
 
+		vertices = converter.cleanArray(vertices);
+		uvs = converter.cleanArray(uvs);
+		faces = converter.cleanArray(faces);
+
+		converter.build(vertices, uvs, faces);
+
 		return false;
 	}
+
+	converter.cleanArray = function(array)
+	{
+		var newArray = [];
+		for(var i = 0; i < array.length; i++)
+		{
+			for(var j = 1; j < array[i].length; j++)
+			{
+				newArray.push(array[i][j]);
+			}
+		}
+		array = newArray;
+
+		for(var i = 0; i < array.length; i++)
+		{
+			var element = array[i];
+			element = Math.round(element / 0.0625) * 0.0625;
+			array[i] = element;
+		}
+		return array;
+	}
+
+	converter.build = function(Vertices, UVs, Faces)
+	{
+		var vertString = "";
+		var uvString = "";
+		var faceString = "";
+
+		for(var i = 0; i < Vertices.length; i++)
+		{
+			switch(i % 3)
+			{
+				case 0:
+					var string = "double x" + (Math.floor(i / 3) + 1) + " = (double)(par2 + " + Vertices[i] + ");\n";
+					vertString = vertString + string;
+				break;
+				case 1:
+					var string = "double y" + (Math.floor(i / 3) + 1) + " = (double)(par3 + " + Vertices[i] + ");\n";
+					vertString = vertString + string;
+				break;
+				case 2:
+					var string = "double z" + (Math.floor(i / 3) + 1) + " = (double)(par4 + " + Vertices[i] + ");\n";
+					vertString = vertString + string;
+				break;
+				default:
+				break;
+			}
+		}
+
+		for(var i = 0; i < UVs.length; i++)
+		{
+			switch(i % 2)
+			{
+				case 0:
+					if(UVs[i] == 0)
+					{
+						var string = "double u" + (Math.floor(i / 2) + 1) + " = (double)icon.getMinU();\n";
+					}
+					else if(UVs[i] == 1)
+					{
+						var string = "double u" + (Math.floor(i / 2) + 1) + " = (double)icon.getMaxU();\n";
+					}
+					else
+					{
+						var string = "double u" + (Math.floor(i / 2) + 1) + " = (double)icon.getInterpolatedU(" + UVs[i] + "D);\n";
+					}
+					uvString = uvString + string;
+				break;
+				case 1:
+					if(UVs[i] == 0)
+					{
+						var string = "double v" + (Math.floor(i / 2) + 1) + " = (double)icon.getMinV();\n";
+					}
+					else if(UVs[i] == 1)
+					{
+						var string = "double v" + (Math.floor(i / 2) + 1) + " = (double)icon.getMaxV();\n";
+					}
+					else
+					{
+						console.log(UVs[i]);
+						var string = "double v" + (Math.floor(i / 2) + 1) + " = (double)icon.getInterpolatedV(" + UVs[i] + "D);\n";
+					}
+					uvString = uvString + string;
+				break;
+				default:
+				break;
+			}
+		}
+
+		for(var i = 0; i < Faces.length; i++)
+		{
+			switch(i % 2)
+			{
+				case 0:
+					var string = "tessellator.addVertexWithUV(x" + Faces[i] + ", y" + Faces[i] + ", z" + Faces[i] + ", ";
+					faceString = faceString + string;
+				break;
+				case 1:
+					var string = "u" + Faces[i] + ", v" + Faces[i] + ");\n";
+					faceString = faceString + string;
+				break;
+				default:
+				break;
+			}
+		}
+
+		var parsedCode =  vertString + "\n" + uvString + "\n" + faceString;
+		document.getElementById('area_02').value = parsedCode;
+
+		return false;
+	}
+
 	return converter;
 })
 (converter || {});
